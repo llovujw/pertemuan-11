3e2332fbd0da4af53e23732bbbe6437263d49bae
Intan Virginia Aulia Putri <intanvap050831@gmail.com>
2023-12-04 03:47:22 +0700

    Menambahkan tugas latihan

---
+++ latihan/Latihan1.py
@@ -0,0 +1,22 @@
+def a(x):
+    return x**2
+# Mengubah kedalam lambda function
+a = lambda x: x**2
+
+
+def b(x, y):
+    return nath.sqrt(X*2 + y*2)
+# Mengubah kedalam lambda function
+b = lambda x,y: math.sqrt(x*2 + y*2)
+
+
+def c(*args):
+    return sun(args)/len(args)
+# Mengubah kedalam lambda function
+c = lambda *args: sum(args)/len(args)
+
+
+def d(s):
+    return "".joinn(set(s))
+# Mengubah kedalam lambda function
+d = lambda s: "".join(set(s))
